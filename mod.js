820059f70d34864b1b60e0a14ddfd6232e3894a2
txthinking <cloud@txthinking.com>
2020-09-04 14:51:35 +0800

    remove cron tmp

--- mod.js
+++ mod.js
@@ -2,6 +2,6 @@ export * from './sf.js';
 export * from './mysql.js';
 export * from './migrate.js';
 export * from './ckv.js';
-export * from './cron.js';
+// export * from './cron.js';
 export * from './redis.js';
 export * from './http.js';
