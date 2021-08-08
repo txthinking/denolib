88f6f96ef2be8b3e25d4f3594d706ba6e4addff7
TxThinking <cloud@txthinking.com>
2021-08-08 17:04:23 +0800

    Update httpserver.js

--- httpserver.js
+++ httpserver.js
@@ -1,151 +1,62 @@
-import { serve, serveTLS } from "https://deno.land/std@0.103.0/http/server.ts";
-import { acceptWebSocket } from "https://deno.land/std@0.103.0/ws/mod.ts";
-import { BufReader } from "https://deno.land/std@0.103.0/io/bufio.ts";
-
-var httpserver = {};
-
-httpserver.cors = '*';
-
-httpserver.before = (r)=>null;
-httpserver.after = (r)=>null;
-
-httpserver.handler = {};
-httpserver.path = (path, f) => httpserver.handler[path] = f
-
-httpserver.wshandler = {};
-httpserver.ws = (path, f) => httpserver.wshandler[path] = f
-
-httpserver.ok = (data) => { return {error: null, data} };
-httpserver.err = (error) => { return {error, data: null} };
-httpserver.notfound = (r)=>httpserver.err('404')
-
-function response(o){this.o={status: o.status, headers: new Headers(o.headers), body: o.body};}
-httpserver.response = (o)=> new response(o)
-
-var debug = false;
-Object.defineProperty(httpserver, 'debug', {
-    get: function(){
-        return debug;
-    },
-    set: function(v){
-        debug = v;
-    },
-});
-var log = (...args) => debug && console.log((new Date()).toString(), ...args)
-
-var handle = async (r) => {
-    httpserver.before(r);
-    log("=>", r.url);
-
-    r.query = {};
-    r.json = null;
-    r.uint8Array = null;
-    r.response = null;
-
-    var i = r.url.indexOf('?');
-    if(i != -1){
-        r.url.substr(i+1).split('&').forEach(v => {if(v.split('=')[0]) r.query[v.split('=')[0]] = v.split('=')[1] ? decodeURIComponent(v.split('=')[1]) : '';});
-    }
-    log(r.url, "query", r.query);
-
-    var headers = {"Content-Type": "application/json"};
-    if(r.headers.get("Origin")){
-        headers["Access-Control-Allow-Origin"] = httpserver.cors;
-        headers["Access-Control-Allow-Methods"] = "GET, POST, PUT, DELETE, HEAD, PATCH";
-        if(r.headers.get("Access-Control-Request-Headers")){
-            headers["Access-Control-Allow-Headers"] = r.headers.get("Access-Control-Request-Headers");
-        }
-        headers["Access-Control-Max-Age"] = 24*60*60;
-    }
-
-    var h = httpserver.handler[r.url.split('?')[0]];
-    if(!h){
-        var wsh = httpserver.wshandler[r.url.split('?')[0]];
-        if(!wsh){
-            try{
-                var o = await httpserver.notfound(r);
-                r.respond(o instanceof response ? o.o : {headers: new Headers(headers), body: JSON.stringify(o)});
-            }catch(e){
-                var o = httpserver.err(e.toString());
-                r.respond(o instanceof response ? o.o : {headers: new Headers(headers), body: JSON.stringify(o)});
+var httpserver = {};                                                                                                                                                                                                                             [2/24]
+                                                             
+httpserver.cors = "*";                                                                                                     
+httpserver.handler = {};                                                                                                   
+httpserver.path = (path, f) => (httpserver.handler[path] = f);                    
+httpserver.notfound = async (r) => {                                                                                       
+    return new Response(`404`, {
+        status: 404,                                                                                                       
+    });          
+};                                       
+                                                             
+async function handle(conn) {       
+    const httpConn = Deno.serveHttp(conn);
+    while (true) {                  
+        try {         
+            const r = await httpConn.nextRequest();
+            var h = httpserver.handler[new URL(r.request.url).pathname];
+            try {      
+                var res;     
+                if (!h) {
+                    res = await httpserver.notfound(r);
+                }
+                if (h) {
+                    res = await h(r);
+                }                 
+                if (r.headers.get("Origin")) {
+                    res.headers.set("Access-Control-Allow-Origin", httpserver.cors);
+                    res.headers.set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, HEAD, PATCH");
+                    if (r.request.headers.get("Access-Control-Request-Headers")) {
+                        res.headers.set("Access-Control-Allow-Headers", r.headers.get("Access-Control-Request-Headers"));
+                    }  
+                    res.headers.set("Access-Control-Max-Age", 24 * 60 * 60);
+                } 
+                await r.respondWith(res);
+            } catch (e) {
+                await r.respondWith(
+                    new Response(`$e`, {
+                        status: 500,
+                    })      
+                );                               
             }
-            r.response = o;
-            log('<=', r.url, o);
-            httpserver.after(r);
-            return;
+        } catch (err) {
+            console.log(err);
+            break;
         }
+    }
+}
+
+httpserver.run = async (port) => {
+    const server = Deno.listen({ port });
+    while (true) {
         try {
-            var ws = await acceptWebSocket({
-                conn: r.conn,
-                bufReader: r.r,
-                bufWriter: r.w,
-                headers: r.headers,
-            });
-        } catch (e) {
-            var o = httpserver.err(e.toString());
-            r.respond(o instanceof response ? o.o : {headers: new Headers(headers), body: JSON.stringify(o)});
-            r.response = o;
-            log('<=', r.url, o);
-            httpserver.after(r);
-            return;
-        }
-        wsh(r, ws).catch(async e=>{
-            console.error("wshandle", r.url, e);
-            if (!ws.isClosed) {
-                await ws.close(1000).catch(console.error);
-            }
-            httpserver.after(r);
-        });
-        return;
-    }
-
-    if(r.contentLength !== null){
-        var b = new Uint8Array(r.contentLength);
-        var br = await new BufReader(r.body);
-        var b1 = await br.readFull(b);
-        if (b1 === null){
-            log(r.url, "body length is", r.contentLength, "but read 0, ignored")
-            return;
-        }
-        r.uint8Array = b;
-        try{
-            r.json = JSON.parse(new TextDecoder().decode(b));
-        }catch(e){
-            log(r.url, "body is not json, you can read it from r.unit8Array");
+            const conn = await server.accept();
+            handle(conn);
+        } catch (err) {
+            console.log(err);
+            break;
         }
     }
-    if(r.json){
-        log(r.url, "body", r.json);
-    }
-
-    try{
-        var o = await h(r);
-        r.respond(o instanceof response ? o.o : {headers: new Headers(headers), body: JSON.stringify(o)});
-    }catch(e){
-        var o = httpserver.err(e.toString());
-        r.respond(o instanceof response ? o.o : {headers: new Headers(headers), body: JSON.stringify(o)});
-    }
-    r.response = o;
-    log('<=', r.url, o);
-    httpserver.after(r);
-}
-
-httpserver.run = async (options) => {
-    if(!options || typeof options === 'number'){
-        var s = serve({
-            port: options || 2020,
-        });
-    }else{
-        var s = serveTLS({
-            port: options.port || 2020,
-            hostname: options.hostname,
-            certFile: options.certFile,
-            keyFile: options.keyFile,
-        });
-    }
-    for await (var r of s) {
-        handle(r).catch(console.error);
-    }
-}
+};
 
 export default httpserver;
