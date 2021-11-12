8597a4f23c5e430a48075a2e4c8d6bca4d4fb57a
TxThinking <cloud@txthinking.com>
2021-11-12 11:52:01 +0800

    Update crypto.js

--- crypto.js
+++ crypto.js
@@ -1,5 +1,4 @@
-import { Aes } from "https://deno.land/x/crypto/aes.ts";
-import { Cfb, Padding } from "https://deno.land/x/crypto/block-modes.ts";
+import { AES } from "https://deno.land/x/god_crypto@v1.4.8/mod.ts";
 import { decodeString as hexdecode } from "https://deno.land/std@0.88.0/encoding/hex.ts";
 import { encodeToString as hexencode } from "https://deno.land/std@0.88.0/encoding/hex.ts";
 import { randomBytes } from "https://deno.land/std@0.85.0/node/crypto.ts";
@@ -17,13 +16,15 @@ var crypto = (key) => {
                 t: parseInt(Date.now() / 1000),
             });
             var iv = randomBytes(16);
-            var b = new Cfb(Aes, new TextEncoder().encode(key), iv, Padding.PKCS7).encrypt(new TextEncoder().encode(text));
+            var kv = new AES(key, { mode: "cfb", iv: iv });
+            var b = await kv.encrypt(text);
             return hexencode(new Uint8Array([...iv, ...b]));
         },
         decrypt: (k, c, lifecycle) => {
             var b = hexdecode(c);
-            var s = new TextDecoder().decode(new Cfb(Aes, new TextEncoder().encode(key), b.slice(0, 16), Padding.PKCS7).decrypt(b.slice(16)));
-            var o = JSON.parse(s);
+            var kv = new AES(key, { mode: "cfb", iv: b.slice(0, 16) });
+            var r = await kv.decrypt(b.slice(16));
+            var o = JSON.parse(r.toString());
             if (lifecycle && o.t + lifecycle < parseInt(Date.now() / 1000)) {
                 return null;
             }
