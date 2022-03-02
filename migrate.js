bdd99ec1fb7ca58db588d98c048710aebceaf08f
TxThinking <cloud@txthinking.com>
2022-03-02 11:31:15 +0800

    Update migrate.js

--- migrate.js
+++ migrate.js
@@ -1,26 +1,20 @@
-import { Client } from "https://deno.land/x/mysql/mod.ts";
-
-var migrate = async (config) => {
-    var db = config.db;
-    delete config.db;
-    var client = await new Client().connect(config);
-
+var migrate = async (db, dbname) => {
     var f =  async (id, sql) => {
-        var r = await client.query(`select * from migration where ID=?`, [id]);
+        var r = await db.query(`select * from migration where ID=?`, [id]);
         if(r.length != 0){
             return
         }
-        await client.execute(sql);
-        await client.execute(`insert into migration values('${id}')`);
+        await db.execute(sql);
+        await db.execute(`insert into migration values('${id}')`);
     };
 
-    await client.execute(`CREATE DATABASE IF NOT EXISTS ${db}`);
-    await client.execute(`USE ${db}`);
-    var r = await client.execute(`show tables like 'migration'`);
+    await db.execute(`CREATE DATABASE IF NOT EXISTS ${dbname}`);
+    await db.execute(`USE ${dbname}`);
+    var r = await db.execute(`show tables like 'migration'`);
     if(r.rows.length != 0 ){
         return f;
     }
-    await client.execute(`
+    await db.execute(`
         CREATE TABLE migration (
             ID varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
             UNIQUE KEY (ID)
